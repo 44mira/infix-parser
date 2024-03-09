40ac6c1ec55c607477c51c986b0f4aaed375752d
44mira <legolaslada@gmail.com>
2024-03-09 21:10:05 +0800

    improved grammar for numeric

--- Mabano_Lada_PE01.cpp
+++ Mabano_Lada_PE01.cpp
@@ -18,8 +18,8 @@
  * expression : term { ("+" | "-") term } .
  * term : factor { ("*" | "/" | "%") factor } .
  * factor : numeric | "(" expression ")" .
- * digit : "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" .
- * numeric : { digit } (digit | "0") .
+ * digit : "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" .
+ * numeric : { digit } digit .
  */
 
 #include <cctype>
