dfdce3d10f7282958913bc00d732382d49adacde
44mira <legolaslada@gmail.com>
2024-03-09 23:44:24 +0800

    created definitions.cpp and moved function definitions

--- Mabano_Lada_PE01.cpp
+++ Mabano_Lada_PE01.cpp
@@ -87,6 +87,8 @@ void evaluateLoop(void) {
     cin.ignore();
     getline(cin, expr);
 
+    // TODO: Parser
+
     cout << "\nDo you want to evaluate another expression?\n\n"
             "[X] NO\n"
             "[other] YES\n"
@@ -96,48 +98,4 @@ void evaluateLoop(void) {
     if (toupper(exit) == EXIT)
       return;
   }
-}
-
-vector<token> lexer(const string expr) {
-  vector<token> tokens;
-  for (int i = 0; i < expr.length(); i++) {
-    if (isspace(expr[i]))
-      continue;
-
-    if (isdigit(expr[i])) {
-      string number = "";
-
-      // collect digits into a NUMBER token
-      while (isdigit(expr[i])) {
-        number += expr[i];
-        i++;
-      }
-      tokens.push_back({NUMBER, number});
-      i--;
-
-      continue;
-    }
-
-    switch (expr[i]) {
-    case '+':
-    case '-':
-      tokens.push_back({PRECEDENCE_1, string() + expr[i]});
-      break;
-    case '*':
-    case '/':
-    case '%':
-      tokens.push_back({PRECEDENCE_2, string() + expr[i]});
-      break;
-    case '(':
-      tokens.push_back({OPEN_PRN, "("});
-      break;
-    case ')':
-      tokens.push_back({CLOSE_PRN, ")"});
-      break;
-    default:
-      tokens.push_back({ERROR, ""});
-    }
-  }
-
-  return tokens;
 }
