9c56f084ea5ca76886150be34e27f2f47ab59550
Sharmaigne Angelie Mabano <sharmaigne@Sharmaignes-MacBook-Air.local>
2024-03-09 21:26:49 +0800

    added main loop and boilerplate (prog desc, evaluation loop)

--- Mabano_Lada_PE01.cpp
+++ Mabano_Lada_PE01.cpp
@@ -22,14 +22,23 @@
  * numeric : { digit } (digit | "0") .
  */
 
-#include <cctype>
+#include <algorithm>
+#include <cctype>   // toUpper
 #include <iostream>
 #include <string>
+#include <vector>
+#include <stdlib.h> // for clearing
+#include <stack>
 
 using namespace std;
 
 enum TYPES { Numeric, Operator };
 typedef int type_data;
+
+enum MENU_CHOICES { EXIT = 'X', PROG_DESC = 'P', EVALUATE = 'E' };
+
+#define choice char
+#define token string
 
 /**
  * `node` type that can be either `Numeric `or `Operator`
@@ -47,6 +56,24 @@ struct node {
 };
 
 /**
+ * Menu interface, returns a choice value based on what user inputs.
+ *
+ * @return menu choice
+ */
+choice menu(void);
+
+/**
+ * Prints out the program description to `stdout`.
+ */
+void programDescription(void);
+
+/**
+ * Enter the loop for evaluating expressions.
+ * Exit back to `main` on 'X' or invalid expression.
+ */
+void evaluateLoop(void);
+
+/**
  * Removes spaces from the input expression as the grammar deals by character.
  * Uses string to hold tokens as they're equivalent to arrays of characters.
  *
@@ -55,7 +82,58 @@ struct node {
  */
 string lexer(const string expression);
 
-int main(void) {}
+int main(void) {
+	bool running = true;
+
+	while (running) {
+		switch (menu()) {
+			case PROG_DESC:
+        programDescription();
+        break;
+			case EVALUATE:
+        evaluateLoop();
+        break;
+			case EXIT:
+        running = false;
+        break;
+			default:
+        cout << "\nInvalid input, try again.\n";
+		}
+	}
+  return 0;
+}
+
+choice menu(void) {
+  choice ret;
+  cout << "\nWelcome to this Expression Evaluator program! Please choose an "
+          "action to perform...\n"
+          "[P] Program Description\n"
+          "[E] Evaluate Expression(s)\n"
+          "[X] Exit\n"
+          "Choice: ";
+  cin >> ret;
+
+  return toupper(ret); // lowercase versions of P E X should be valid
+}
+
+void programDescription(void) {
+  cout << "\n\n\t\t==== PROGRAM DESCRIPTION ====\n\n"
+          "Legolas Tyrael B. Lada       | 2022-04734\n"
+          "Sharmaigne Angelie C. Mabano | 2022-03464\n"
+          "March 05, 2024 - March 09, 2024\n=====\nMenu Descriptions:\n\n"
+
+          "- [P] Program Description: Prints out the program description to "
+          "stdout.\n"
+          "- [E] Evaluate Expression(s): Takes an infix expression as input "
+          "and displays its equivalent postfix expression and its result.\n"
+          "- [X] Exit: Terminates the program.\n\n=====\n"
+          
+          "Work distribution:\n"
+          "Tyrael: Code structure setup, infix to postfix conversion\n"
+          "Sharmaigne: Input validation, postfix expression evaluation\n\n"
+          "\t\t============================\n\n";
+
+}
 
 string lexer(const string expression) {
   string tokens = "";
@@ -63,4 +141,23 @@ string lexer(const string expression) {
     if (!isspace(token))
       tokens += token;
   return tokens;
+}
+
+void evaluateLoop(void) {
+	string expr;
+	choice exit;
+	while (true) {
+		cout << "\n Input an infix expression: ";
+		cin.ignore();
+		getline(cin, expr);
+
+		cout << "\nDo you want to evaluate another expression?\n\n"
+						"[X] NO\n"
+						"[other] YES\n"
+						"Choice: ";
+
+		cin >> exit;
+		if (toupper(exit) == EXIT)
+			return;
+	}
 }
