c1d47b8a8031059910e798204028c955b6987414
44mira <legolaslada@gmail.com>
2024-03-09 23:24:19 +0800

    implement lexer

--- Mabano_Lada_PE01.cpp
+++ Mabano_Lada_PE01.cpp
@@ -98,9 +98,44 @@ void evaluateLoop(void) {
   }
 }
 
-vector<token> lexer(const string expression) {
-  for (int i = 0; i < expression; i++) {
+vector<token> lexer(const string expr) {
+  vector<token> tokens;
+  for (int i = 0; i < expr.length(); i++) {
 
-    expression[i]
+    if (isdigit(expr[i])) {
+      string number = "";
+
+      // collect digits into a NUMBER token
+      while (isdigit(expr[i])) {
+        number += expr[i];
+        i++;
+      }
+      tokens.push_back({NUMBER, number});
+      i--;
+
+      continue;
+    }
+
+    switch (expr[i]) {
+    case '+':
+    case '-':
+      tokens.push_back({PRECEDENCE_1, string() + expr[i]});
+      break;
+    case '*':
+    case '/':
+    case '%':
+      tokens.push_back({PRECEDENCE_2, string() + expr[i]});
+      break;
+    case '(':
+      tokens.push_back({OPEN_PRN, "("});
+      break;
+    case ')':
+      tokens.push_back({CLOSE_PRN, ")"});
+      break;
+    default:
+      tokens.push_back({ERROR, ""});
+    }
   }
+
+  return tokens;
 }
